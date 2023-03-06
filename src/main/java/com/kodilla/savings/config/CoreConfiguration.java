53b4f7fbb4f440d40465a7145361a0b50830dc1b
Tomasz Lenczewski <lenczewski.tom@gmail.com>
2023-03-06 18:48:22 +0100

    CoreConfiguration fix

--- src/main/java/com/kodilla/savings/config/CoreConfiguration.java
+++ src/main/java/com/kodilla/savings/config/CoreConfiguration.java
@@ -3,6 +3,8 @@ package com.kodilla.savings.config;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.builders.PathSelectors;
@@ -10,6 +12,10 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.beans.factory.annotation.Value;
+
+
+import java.util.Properties;
 
 @Configuration
 @EnableScheduling
@@ -17,6 +23,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class CoreConfiguration {
 
+    @Value("${MAIL_USERNAME}")
+    private String usernameMail;
+    @Value("${MAIL_PASSWORD}")
+    private String usernamePassword;
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
@@ -29,5 +39,23 @@ public class CoreConfiguration {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.mailtrap.io");
+        mailSender.setPort(2525);
+
+        mailSender.setUsername(usernameMail);
+        mailSender.setPassword(usernamePassword);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
     }
 }
